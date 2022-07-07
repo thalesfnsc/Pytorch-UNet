627295d040d2d2ab698ad51441d8105bd6509761
Thales Fonseca <49501025+thalesfnsc@users.noreply.github.com>
2022-07-07 09:56:44 -0300

    Update data_loading.py

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -63,17 +63,25 @@ class BasicDataset(Dataset):
         mask = self.load(mask_file[0])
         img = self.load(img_file[0])
 
-        assert img.size == mask.size, \
-            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+
 
         img = self.preprocess(img, self.scale, is_mask=False)
         mask = self.preprocess(mask, self.scale, is_mask=True)
+ 
+        if(mask.shape != img.shape[1:]):
+          mask = mask.T
+       
+        assert img.shape[1:] == mask.shape, \
+            f'Image and mask {name} should be the same size, but are {img.shape[1:]} and {mask.shape}'
 
+        #print("mask shape:",mask.shape)
+        #print("img shape:", img.shape)
+        
         #changing mask values
         mask = np.where(mask == 127,1,mask)
         mask = np.where(mask == 255,2,mask)
-        print(img.shape)
-
+        #print(img.shape)
+        
         return {
             'image': torch.as_tensor(img.copy()).float().contiguous(),
             'mask': torch.as_tensor(mask.copy()).long().contiguous()
